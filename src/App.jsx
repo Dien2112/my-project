196e0f2a8373c11e6a6af4392f254398cf321304
Dien2112 <116195602+Dien2112@users.noreply.github.com>
2024-12-04 23:00:13 +0700

    Value

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,19 @@ function App() {
 
   return (
     <div class="bg-gradient-to-b from-green-400 to-blue-400 w-screen h-screen flex items-center justify-center">
-      <div class= "bg-white w-[500px] h-[500px] items-start justify-start">
+      <div class= "bg-white w-[500px] h-[500px] flex flex-col items-start justify-start">
           <div class="flex items-center ">
-            <img class="h-[80px] w-[80px]" src="public/vite.svg" alt="image description"/>
+            <img class="h-[80px] w-[80px]" src="public/icons.png" alt="image description"/>
             <p class="font-bold font-mono">LibraryCore</p>
           </div>
 
         <p class="font-mono size-6 ml-5">Login</p>
 
-        <input class="items-start ml-5" placeholder="username"/>
+        <input class="input-underline items-start ml-5" placeholder="username"/>
 
-        <input class="item-stert ml=5" type = "password" placeholder="password"/>
+        <input class="input-underline item-stert ml-5" type = "password" placeholder="password"/>
 
-        <p>Do not have a password? <href>Create an account</href></p>
+        <p>Do not have a password? <a href="#" class="text-blue-500">Create an account</a></p>
 
       </div>
 
